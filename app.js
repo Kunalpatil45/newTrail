f6b8f979f7a6dc3a2cefb96d5be2a77d1c43ab4a
kunalpatil4795 <patilkunal4795@gmail.com>
2025-04-08 10:50:40 +0530

    newfeatures8/04

---
+++ app.js
@@ -0,0 +1,6 @@
+const oranges = document.querySelector('.oranges');
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('dblclick', () => {
+    oranges.style.display = "block";
+})
